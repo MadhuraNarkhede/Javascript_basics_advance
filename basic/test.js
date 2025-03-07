a3cd1ffbe313657b407ae65ed1d23b689902917c
Madhura Lahu  Narkhede <156207107+MadhuraNarkhede@users.noreply.github.com>
2025-03-07 13:08:04 +0000

    main

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,7 @@
-console.log("Madhura Lahu Narkhede");
+const name_e = " Madhura ";
+let password = "1234"
+email = "abc@gmail.com"
+let edc;
+
+console.log(name_e);
+console.table([name_e,password,email,edc]);
