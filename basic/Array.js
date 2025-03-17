21809dfe0b923e85f571b752b9168fd0f26b0e84
Madhura Lahu  Narkhede <156207107+MadhuraNarkhede@users.noreply.github.com>
2025-03-17 09:27:35 +0530

    Update Array.js

--- basic/Array.js
+++ basic/Array.js
@@ -74,15 +74,15 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
+console.log(Array.isArray("Hitesh"))//tells whether the pass argument is array or not.
+console.log(Array.from("Hitesh"))//mkaes the pass argument into array .
 console.log(Array.from({name: "hitesh"})) // interesting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3));// converts various single elements to array.
 
 
 
