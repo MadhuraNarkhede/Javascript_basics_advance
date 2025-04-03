39e739fc8d3c377acf962db2a8c54a1922a26478
Madhura Lahu  Narkhede <156207107+MadhuraNarkhede@users.noreply.github.com>
2025-04-03 04:11:33 +0000

    swirch

---
+++ 04_controlFLow.js/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+//If the "keyword" is removed then it will execute all the code after that value if match except the default case, thus default case will not be executed if break keyword is removed.
