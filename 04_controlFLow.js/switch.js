544ac4372a5e756c8e83284b712cb15a513b37a2
Madhura Lahu  Narkhede <156207107+MadhuraNarkhede@users.noreply.github.com>
2025-04-03 04:15:30 +0000

    switch

--- 04_controlFLow.js/switch.js
+++ 04_controlFLow.js/switch.js
@@ -7,19 +7,41 @@
 //         break;
 // }
 
-const month = "march"
+// const month = "march"
+
+// switch (month) {
+//     case "jan":
+//         console.log("January");
+//         break;
+//     case "feb":
+//         console.log("feb");
+//         break;
+//     case "march":
+//         console.log("march");
+//         break;
+//     case "april":
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// // }
+//If the "keyword" is removed then it will execute all the code after that value if match except the default case, thus default case will not be executed if break keyword is removed.
+
+const month = 1
 
 switch (month) {
-    case "jan":
+    case 1:
         console.log("January");
         break;
-    case "feb":
+    case 2:
         console.log("feb");
         break;
-    case "march":
+    case 3:
         console.log("march");
         break;
-    case "april":
+    case 4:
         console.log("april");
         break;
 
@@ -27,4 +49,3 @@ switch (month) {
         console.log("default case match");
         break;
 }
-//If the "keyword" is removed then it will execute all the code after that value if match except the default case, thus default case will not be executed if break keyword is removed.
