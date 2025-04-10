5e81b2bb4e4578c2ffa88c71b88f42f5417cf05b
Madhura Lahu  Narkhede <156207107+MadhuraNarkhede@users.noreply.github.com>
2025-04-10 05:15:23 +0000

    update

--- 07_project/Digital Clock/javascript.js
+++ 07_project/Digital Clock/javascript.js
@@ -1,6 +1,6 @@
 const clock = document.getElementById('clock');
 
-setInterval(function(){
+setInterval(function(){//setInterval( function(){ function defination} , interval(in miliseconds)) it alllows us to exexute the a particular function or a code at a intervals , mention in the function.
     const date = new Date();
     clock.textContent=date.toLocaleTimeString();
 },1000)
